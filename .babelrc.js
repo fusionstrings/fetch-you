cd4e582e9b69efd6ef3a3333c7b8a3e2f979927c
Dilip Kr. Shukla <contact@fusionstrings.com>
2018-04-05 12:00:40 +0200

    remove external-helper plugin as it's deprecated by rollup-plugin-babel

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,3 @@
 module.exports = {
-	"presets": [["@fusionstrings/fusionstrings", {modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false}]],
-	"plugins": process.env.NODE_ENV === 'test' ? [] : ["@babel/plugin-external-helpers"]
+	"presets": [["@fusionstrings/fusionstrings", {modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false}]]
 }
